8ecd3064f41e0db03ec1f30e1c9fe1b717cd2fa3
sarita595k <sarita595k@gmail.com>
2025-06-08 21:15:46 +0530

    small commit

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 
+// dotenv file config
 dotenv.config();
 const app = express();
 
@@ -13,6 +14,6 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-// Port
+// Port created
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
